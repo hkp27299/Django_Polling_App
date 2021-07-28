6a40a909600ba7612431ff2b19fa5f76429b1f00
Harsh <hkp27299@gmail.com>
2021-07-28 19:39:39 +0530

    deployment changes

--- poll/urls.py
+++ poll/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
 from .views import signUp,signIn,createPollView,viewPollView,vote
-
+from django.contrib import admin
 urlpatterns = [
     path('signup/', signUp,name='signup'),
     path('signin/', signIn,name='signin'),
     path('create/', createPollView,name='create'),
     path('view/', viewPollView,name='view'),
     path('vote/<str:title>/<str:selectedoption>', vote, name='vote'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
--- polling/settings.py
+++ polling/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hn#p2u-cpv8k^@)z2vj2h^g8friw_d@a1gj#mqm7uo25e_p2_a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
